d7fa13a5528714cec9bdf51cf6144d20f7f15127
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-27 01:40:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,4 +8,13 @@ def loop_message_five_times(message)
   end
 end
 
-def loop_message_n_times(message, )
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do 
+    puts message 
+    count += 1 
+  end
+end
+
+
+  
