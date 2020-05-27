f1bf66def4f9d67ef2bbbee1bd4e0834f1ee6ec8
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-27 01:49:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,7 +18,7 @@ end
 
 def output_array(array)
   count = 0 
-  while count < array.lenght do
+  while count < array.length do
     puts array[count]
     count += 1 
   end
@@ -26,7 +26,7 @@ end
 
 def return_string_array(array)
   count = 0 
-  while count < array.lenght do
+  while count < array.length do
     puts array[count].to_s 
     count += 1 
   end
