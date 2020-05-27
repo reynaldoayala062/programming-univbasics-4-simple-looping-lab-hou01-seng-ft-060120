62b87ff3a68f58ec387e39204bd7d83288e7b55b
Reynaldo Ayala <reynaldo-ayala@hotmail.com>
2020-05-27 01:47:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,5 +16,20 @@ def loop_message_n_times(message, limit)
   end
 end
 
+def output_array(array)
+  count = 0 
+  while count < array.lenght do
+    puts array[count]
+    count += 1 
+  end
+end
+
+def return_string_array(array)
+  count = 0 
+  while count < array.lenght do
+    puts array[count].to_s 
+    count += 1 
+  end
+end
 
   
